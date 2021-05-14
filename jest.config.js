da5b306cee5bca5f9359b49d03121cc57b98258e
Marcelo Barbosa <barbosa.marcelo6@gmail.com>
2021-05-14 09:58:07 -0300

    feat(config): configure typescript, babel, jest, and create structure of project folders

---
+++ jest.config.js
@@ -0,0 +1,29 @@
+// jest.config.js
+const { defaults: tsjPreset } = require('ts-jest/presets');
+
+module.exports = {
+  ...tsjPreset,
+  preset: 'react-native',
+  verbose: true,
+  automock: false,
+  collectCoverage: false,
+  coverageThreshold: {
+    global: {
+      // @TODO: Revisitar esse ponto quando tivermos tempo de criar testes
+      branches: 0,
+      functions: 20,
+      lines: 80,
+      statements: 80,
+    },
+  },
+  transform: {
+    ...tsjPreset.transform,
+    '\\.js$': '<rootDir>/node_modules/react-native/jest/preprocessor.js',
+  },
+  globals: {
+    'ts-jest': {
+      babelConfig: true,
+    },
+  },
+  cacheDirectory: '.jest/cache',
+};
