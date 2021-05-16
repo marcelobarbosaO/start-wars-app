9ce80df9343449cd2660cc4d47f1b0f2a5575402
Marcelo Barbosa <barbosa.marcelo6@gmail.com>
2021-05-16 11:03:56 -0300

    feat(tests): add components test

--- __tests__/components/EmptyComponent.test.js
+++ __tests__/components/EmptyComponent.test.js
@@ -2,8 +2,8 @@ import 'react-native';
 import * as React from 'react';
 import renderer from 'react-test-renderer';
 
-import App from '../src';
+import EmptyContent from '../../src/components/EmptyContent';
 
 it('renders correctly', () => {
-  renderer.create(<App />);
+  renderer.create(<EmptyContent text="teste" />);
 });
